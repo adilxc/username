551385f769a09ec26ef0120af1ad9ed6b9c433b3
adilxc <mosabhai421@gmail.com>
2026-07-17 11:55:20 +0530

    Working Django + DRF + Postgres + Docker setup

--- logiii/settings.py
+++ logiii/settings.py
@@ -73,14 +73,16 @@ WSGI_APPLICATION = 'logiii.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 # Password validation
